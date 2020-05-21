019222465c27fea559863852b461555e11842cb7
pravin-karale <pravinkarale21@gmail.com>
2020-05-21 19:54:37 +0530

    first commit containing application setup, database and environment variable setup

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Ruby version used :- 2.4.3
+Rails version used :- 5.2.4.3 
+database used :- mysql
+start mysql service by below command
+  service mysql start
+
+kindly rename file config/local_env.yaml_example to config/local_env.yaml 
+
+To save enironment variable, we maintian this file named local_env.yml
+kindly update variables value with your credentials. 
---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
+module ContactMessageFinal
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+  end
+end
