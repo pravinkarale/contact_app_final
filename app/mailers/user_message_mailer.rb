3abc8bac0e269d8c59debad1449944d021a60a39
pravin-karale <pravinkarale21@gmail.com>
2020-05-22 07:20:08 +0530

    missing name in mamiler subject. corrected

--- app/mailers/user_message_mailer.rb
+++ app/mailers/user_message_mailer.rb
@@ -1,6 +1,6 @@
 class UserMessageMailer < ApplicationMailer
   def user_message(opts)
   	@contact_message = opts 
-    mail(subject: "#{opts[:full_name]} sent you the message")
+    mail(subject: "#{@contact_message.full_name} sent you the message")
   end
 end
